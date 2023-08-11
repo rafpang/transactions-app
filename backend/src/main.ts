608977f19f29fabba083d3b03e7a3f003ccd2d2a
rafpang <rafibintangpagi@gmail.com>
2023-08-11 17:36:55 +0700

    fixed cors on nest js

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import * as cors from "cors";
+
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.enableCors({
+    origin: "http://localhost:5173", // Change this to match your Vite app URL
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3333);
 }
